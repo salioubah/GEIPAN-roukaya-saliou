9835d14b308e56f423a6ede44e72c7b8135c2702
bensalah-Roukaya <bensalahroukaya@gmail.com>
2020-01-28 10:47:10 +0100

    first config

--- serveur/serverCrudWithMongo.js
+++ serveur/serverCrudWithMongo.js
@@ -1,6 +1,6 @@
 const express  = require('express');
 const app      = express();
-const port     = process.env.PORT || 8080;
+const port     = process.env.PORT || 8081;
 const server   = require('http').Server(app);
 // pour les formulaires multiparts
 var multer = require('multer');
