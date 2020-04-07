69a6d6020033d4f14c8be57f9e844a4bf7941324
bensalah-Roukaya <bensalahroukaya@gmail.com>
2020-04-08 01:05:57 +0200

    Add router detailsCas

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import DetailsCas from "./components/detailsCas/DetailsCas";
+import ReactDOM from 'react-dom';
+import {UIRouter, pushStateLocationPlugin} from "@uirouter/react";
+import CasTable from "./components/table/CasTable";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let defaultState = {name: 'default', url: '/', component: CasTable};
+let detailsCasState = {name: 'detailsCas', url: '/detailsCas', component: DetailsCas};
+
+ReactDOM.render(
+    <UIRouter
+        plugins={[pushStateLocationPlugin]}
+        states={[detailsCasState, defaultState]}
+    >
+        <App/>
+    </UIRouter>
+
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
